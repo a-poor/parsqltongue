8bc5171cbe73461061bfba387623145b24a346c0
a-poor <a-poor@users.noreply.github.com>
2022-02-18 11:04:07 -0800

    Staritng interface structure

--- parsqltongue.go
+++ parsqltongue.go
@@ -7,3 +7,44 @@ import (
 type Parser struct {
 	p *participle.Parser
 }
+
+func NewParser() *Parser {
+	return &Parser{
+		p: participle.MustBuild(&SelectStatement{}),
+	}
+}
+
+type SelectStatement struct {
+	Select  []*SelectVal
+	From    []*FromVal
+	Where   []*WhereVal
+	GroupBy []*GroupByVal
+	Having  []*HavingVal
+	OrderBy []*OrderByVal
+	Limit   []*LimitVal
+	Offset  []*OffsetVal
+}
+
+type SelectVal struct {
+}
+
+type FromVal struct {
+}
+
+type WhereVal struct {
+}
+
+type GroupByVal struct {
+}
+
+type HavingVal struct {
+}
+
+type OrderByVal struct {
+}
+
+type LimitVal struct {
+}
+
+type OffsetVal struct {
+}
